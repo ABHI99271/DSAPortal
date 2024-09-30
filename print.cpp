4f01948a156ee69c5e7251556b3477fa4a0d6935
ABHI99271 <abhishekrana99271@gmail.com>
2024-09-30 07:36:32 +0530

    mene new file banaya hai print.cpp

---
+++ print.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cout<<"enter a: ";
+    cin>>a;
+
+    int b ;
+    cout<<"enter b: " ;
+    cin>>b;
+
+    cout<<"product of a and b is :"<<a*b;
+
+
+}
